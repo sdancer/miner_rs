664960304dc41a747d80e8712300eaa4a21824c8
lissana <ktion23@gmail.com>
2025-09-02 09:38:26 +0400

    .

--- src/miner.cu
+++ src/miner.cu
@@ -575,12 +575,12 @@ void solve_nonce_range_fused(
         __syncthreads();
 
 
-        if (i == 0 && j == 0 && seed == 0) {
-            for (int a = 0; a < 64; a++) {
-           d_hashes[a] = tileC[a];
-            }
-        }
-        __syncthreads();
+        //if (i == 0 && j == 0 && seed == 0) {
+        //    for (int a = 0; a < 64; a++) {
+        //   d_hashes[a] = tileC[a];
+        //    }
+        //}
+        //__syncthreads();
 
 
         // Hash the 1024 bytes (16 × 64B blocks) → write 32B per seed
