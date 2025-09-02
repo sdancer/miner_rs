d8e86f46ca7ca388635013f1bba8e1cdaeaabdb9
lissana <ktion23@gmail.com>
2025-09-02 09:17:10 +0400

    .

--- src/miner.cu
+++ src/miner.cu
@@ -573,9 +573,6 @@ void solve_nonce_range_fused(
         tileC[i * 16 + j] = acc;
         __syncthreads();
 
-        __syncthreads();
-
-
         // Hash the 1024 bytes (16 × 64B blocks) → write 32B per seed
         //if (i == 0 && j == 0) {
         //    u32 cv[8];      // chaining value
